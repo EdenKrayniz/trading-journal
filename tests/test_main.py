5a4dfd3372ebe67f64be617bcff9a4b19c736301
Eden Krayniz <edenkra77@gmail.com>
2026-09-08 00:29:05 +0300

    Restore test assertion

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,4 +5,4 @@ from app.main import app
 def test_health():
     client = TestClient(app)
     response = client.get("/health")
-    assert response.status_code == 500
+    assert response.status_code == 200
